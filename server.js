ab029d7d6c2bdefc2639d83de6420c9629c174aa
ngalande <blessngalande@gmail.com>
2026-01-03 12:29:46 -0600

    update

--- server.js
+++ server.js
@@ -44,12 +44,11 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: '*', // Allow all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    allowedHeaders: ['sessionId', 'Content-Type', 'master-token'], // Allowed headers
-    exposedHeaders: ['sessionId'], // Headers exposed to the browser
-    preflightContinue: false, // Don't pass the CORS preflight response to the next handler
-    optionsSuccessStatus: 204 // Response status for successful OPTIONS requests
+    origin: '*',
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
 }));
 
 app.get("/", (req, res) => {
